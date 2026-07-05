081cb1ff7130e1209832abb6dbbe2b83e478199d
ehteshamalvi60 <ehteshamalvi60@gmail.com>
2026-07-05 17:33:05 +0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD7v20Zhux0mXbzi53Ys9JECOOnDsKEPEA",
+  authDomain: "eacl-8301b.firebaseapp.com",
+  projectId: "eacl-8301b",
+  storageBucket: "eacl-8301b.firebasestorage.app",
+  messagingSenderId: "222046405705",
+  appId: "1:222046405705:web:81022e5aee4c082ad72b98",
+  measurementId: "G-JMZ4PKQL98"
+};
+
+export const app = initializeApp(firebaseConfig);
